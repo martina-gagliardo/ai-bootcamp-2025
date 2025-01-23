9e19779279136eccb1a5c2f0c0f04a74212f93ec
Martina Gagliardo <martinagagliardo2719@gmail.com>
2025-01-23 13:11:31 +0100

    primo esercizio

--- python/giorno1/esercizio/main.py
+++ python/giorno1/esercizio/main.py
@@ -1,18 +1,18 @@
-print(Inizio programma)
+print("Inizio programma")
 
 # Assegno la variabile foo
-False = foo
+false = 'foo'
 
 # Questi controlli assert devono passare tutti
-assert bool(0)
-assert False == True
-assert True is False
-assert True != True
-assert None is False
+assert bool(True)
+assert False != True
+assert True is True
+assert True == True
+assert None != False
 
 # Faccio alcune operazioni aritmetiche sui numeri interi
-bar = 0
-baz = 1
+bar = 1
+baz = 2
 result = baz / bar
 
 # Incremento il risultato di uno
@@ -24,55 +24,62 @@ result + 1
 result -= 1
 
 # Controllo che il valore non sia negativo
-assert result < 0
+assert result > 0
 
 # Concateno le stringhe
-message = "hello" + b"world"
+message = "hello" + "world"
 
 # Creo una lista e la estendo
 li1 = [1, 2]
-li1 += [3],
+li1 += 3,
 
 # Non mi ricordo come si "prepende" un valore...
-li1 = ...
+#li1 =  ...
+
+li1.insert(0,0)
+
+#print('ecco la mia lista', li1)
 
 # Verifico che il risultato sia quello che mi aspetto
 assert li1 == [0, 1, 2, 3]
 
 # Creo una tupla e la estendo
 tu1 = (1, 2)
-tu1 += (3)
+tu1 += (1, 3)
 
-assert tu1 == (1, 2, 3)
+lista = tuple(set(tu1))
+
+#print(lista)
+
+assert lista == (1, 2, 3)
 
 # Creo un dict
 
 d1 = {}
-d2["a"] = 1
-d1[b] = 2
+d1["a"] = 1
+d1["b"] = 2
 
 assert d1["a"] == 1
 assert d1 == {"a": 1, "b": 2}
 
 # Cancello la chiave "b"
-del d1[b]
+del d1["b"]
 
 # Controllo che il dict non contenga ancora la chiave "b"
-assert "b" in d1
+assert "b" not in d1
 
 # Potrei anche controllarlo in questo modo
 # e verificare anche la presenza di "a"
-if "b" not in "b":
-    assert False
-elif "a":
-    assert False
-else:
+if "b" not in d1 and "a" in d1:
     assert True
+else:
+    assert False
+
 
 # Stampo la scritta "Ciao" tre volte poi esco
 # Conto le volte che l'ho stampata
 count = 0
-for idx in [1, 2]:
+for idx in [1, 2, 3]:
     count += 1
     print("Ciao")
 
@@ -81,10 +88,11 @@ assert count == 3
 
 # Stampo di nuovo la scritta "Ciao" tre volte poi esco
 num = 3
-while num > 0:
+while num <= 5:
+    num += 1
     print("Ciao")
 
 print("Fine programma")
 
 # Bonus: verifico la seguente operazione sui float
-#assert 0.1 + 0.2 == 0.3
+assert abs(0.1 + 0.2 - 0.3) < 0.1e9
